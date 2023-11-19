5f5eb94086b3863f6be31b28370d8674fcaa921a
Giovane <giovanedaniell@gmail.com>
2023-11-19 08:56:39 -0300

    feat: add useCallback to useErrors hook

--- front-end/src/hooks/useErrors.js
+++ front-end/src/hooks/useErrors.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
+  const setError = useCallback(({ field, message }) => {
     const errorAlreadyExists = errors.find((error) => error.field === field);
 
     if (errorAlreadyExists) return;
@@ -12,17 +12,18 @@ export default function useErrors() {
       ...prevState,
       { field, message },
     ]);
-  }
+  }, [errors]);
 
-  function removeError(fieldName) {
+  const removeError = useCallback((fieldName) => {
     setErrors((prevState) => prevState.filter(
       (error) => error.field !== fieldName,
     ));
-  }
+  }, []);
 
-  function getErrorMessageByFieldName(fieldName) {
-    return errors.find(({ field }) => field === fieldName)?.message;
-  }
+  const getErrorMessageByFieldName = useCallback(
+    (fieldName) => errors.find(({ field }) => field === fieldName)?.message,
+    [errors],
+  );
 
   return {
     errors, setError, removeError, getErrorMessageByFieldName,
